de75d410db65eafa2a0afe986a19a3958e526180
MohdIbrahim110 <vikkyrizvi110@gmail.com>
2024-11-22 09:47:11 +0530

    Add files via upload

---
+++ prog3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int array[]={2,5,6,9,8,9,6,3,1,4,8,9,6,8,6,5,4,47};
+	
+	
+	
+	int sizeofArray=sizeof(array)/sizeof(int);
+	
+	cout<<"Size of Array: "<<sizeofArray;
+}
